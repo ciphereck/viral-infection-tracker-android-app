670d2c8538ff078451c9d19c431c5889e644ffc3
ashishbasantani <ashishbasantani77@gmail.com>
2020-04-05 15:28:42 +0530

    Updates splash screen layout

--- app/src/main/java/com/ciphereck/viralinfectiontracker/SplashScreen.java
+++ app/src/main/java/com/ciphereck/viralinfectiontracker/SplashScreen.java
@@ -2,13 +2,11 @@ package com.ciphereck.viralinfectiontracker;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import static java.lang.Thread.sleep;
-
 
 public class SplashScreen extends AppCompatActivity {
     private static final int splashScreenWaitTimeInMS = 2000;
@@ -17,13 +15,10 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
         runAllThreads();
-
-
     }
 
     private void runAllThreads() {
