0c65e531d036b98e514bc6c18c4bad182b66e43d
Mudit Jain <ciphereck@gmail.com>
2020-04-05 14:46:22 +0530

    Adds Splash Screen Functioning and Support for Java 8
    [Summary]
    Added functionality for Splash Screen
    Need to add design for Splash Screen
    Added Cold Start for better user experience
    Added Support for Java 8 (for lambda functions)

---
+++ app/src/main/java/com/ciphereck/viralinfectiontracker/SplashScreen.java
@@ -0,0 +1,45 @@
+package com.ciphereck.viralinfectiontracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import static java.lang.Thread.sleep;
+
+
+public class SplashScreen extends AppCompatActivity {
+    private static final int splashScreenWaitTimeInMS = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        runAllThreads();
+
+
+    }
+
+    private void runAllThreads() {
+        Thread changeScreenThread = new Thread(() -> changeScreen());
+
+        changeScreenThread.start();
+    }
+
+    public void changeScreen() {
+        try {
+            sleep(splashScreenWaitTimeInMS);
+        } catch(Exception e) {
+
+        }
+
+        startActivity(new Intent(SplashScreen.this, MainActivity.class));
+        finish();
+    }
+}
