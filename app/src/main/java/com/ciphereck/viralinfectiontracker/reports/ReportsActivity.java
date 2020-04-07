a81950cd52772916b55436fc6fe197474809e62f
Mudit Jain <ciphereck@gmail.com>
2020-04-07 17:39:28 +0530

    Reports Home Page
    Signed-off-by: Mudit Jain <ciphereck@gmail.com>

--- app/src/main/java/com/ciphereck/viralinfectiontracker/reports/ReportsActivity.java
+++ app/src/main/java/com/ciphereck/viralinfectiontracker/reports/ReportsActivity.java
@@ -7,20 +7,38 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.ciphereck.viralinfectiontracker.R;
 
 public class ReportsActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
+    private TextView totalCases;
+    private TextView totalTestsPerformed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.statistics_list);
-        initRecyclerView();
-        System.out.println(StatisticsClientInstance.getInstance().getStateStatistics().get(0).getTotalCases());
+        setContentView(R.layout.activtity_reports_main);
+        initView();
+    }
+
+    private void initView() {
+        totalCases = findViewById(R.id.totalCases);
+        totalTestsPerformed = findViewById(R.id.totalTest);
+
+        updateData();
+    }
+
+    private void updateData() {
+        if(StatisticsClientInstance.getInstance().getCountryStatistics().size() == 0) {
+            return;
+        }
+        CountryStatistics countryStatistics = StatisticsClientInstance.getInstance().getCountryStatistics().get(0);
+        totalCases.setText(countryStatistics.getTotalCases().toString());
+        totalTestsPerformed.setText(countryStatistics.getTotalTestsPerformed().toString());
     }
 
     private void initRecyclerView() {
