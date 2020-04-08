9b91e07460b26b69ed23aa875b2964df615cb605
ashishbasantani <ashishbasantani77@gmail.com>
2020-04-08 15:25:03 +0530

    Adds search bar in reports

--- app/src/main/java/com/ciphereck/viralinfectiontracker/reports/ReportsActivity.java
+++ app/src/main/java/com/ciphereck/viralinfectiontracker/reports/ReportsActivity.java
@@ -1,11 +1,15 @@
 package com.ciphereck.viralinfectiontracker.reports;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
 
 import com.ciphereck.viralinfectiontracker.R;
@@ -13,12 +17,16 @@ import com.ciphereck.viralinfectiontracker.R;
 public class ReportsActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
+    private Toolbar appBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.statistics_list);
+        appBar=findViewById(R.id.app_bar_reports);
+        setSupportActionBar(appBar);
+
         initRecyclerView();
     }
 
@@ -31,5 +39,14 @@ public class ReportsActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
+        getMenuInflater().inflate(R.menu.search_bar,menu);
+        MenuItem menuItem = menu.findItem(R.id.app_bar_search);
+        SearchView searchView = (SearchView)menuItem.getActionView();
+        searchView.setQueryHint("Search Country");
+
+        return super.onCreateOptionsMenu(menu);
+    }
 }
